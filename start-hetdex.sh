062d2b536f9b735e047fc98c3cd609771c33bc0c
Erin Mentuch Cooper <erin.jm.cooper@gmail.com>
2026-05-05 13:27:01 -0400

    No longer need this startup script

--- start-hetdex.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-mkdir -p /home/jovyan/.jupyter
-chmod 770 /home/jovyan/.jupyter
-touch /home/jovyan/.jupyter/mcp_settings.json
-chmod 660 /home/jovyan/.jupyter/mcp_settings.json
-exec "$@"
-
